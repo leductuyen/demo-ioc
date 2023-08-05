cdcfece9b3b7b05a4a52a4982dbe6aefa8214bbd
Tuyen Le <tuyenld@dtsgroup.com.vn>
2023-08-06 03:27:20 +0700

    updateeeeee 6-8

--- src/views/TongQuan/TongQuanChung.vue
+++ src/views/TongQuan/TongQuanChung.vue
@@ -420,45 +420,7 @@ export default {
             },
 
             selectedValue: {
-                selectedValueUnitEducation: [
-                    {
-                        value: '080',
-                        title: 'Phòng Giáo dục và Đào tạo Thành phố Lào Cai'
-                    },
-                    {
-                        value: '082',
-                        title: 'Phòng Giáo dục và Đào tạo Huyện Bát Xát'
-                    },
-                    {
-                        value: '083',
-                        title: 'Phòng Giáo dục và Đào tạo Huyện Mường Khương'
-                    },
-                    {
-                        value: '084',
-                        title: 'Phòng Giáo dục và Đào tạo Huyện Si Ma Cai'
-                    },
-                    {
-                        value: '085',
-                        title: 'Phòng Giáo dục và Đào tạo Huyện Bắc Hà'
-                    },
-                    {
-                        value: '086',
-                        title: 'Phòng Giáo dục và Đào tạo Huyện Bảo Thắng'
-                    },
-                    {
-                        value: '087',
-                        title: 'Phòng Giáo dục và Đào tạo Huyện Bảo Yên'
-                    },
-                    {
-                        value: '088',
-                        title: 'Phòng Giáo dục và Đào tạo Huyện Sa Pa'
-                    },
-                    {
-                        value: '089',
-                        title: 'Phòng Giáo dục và Đào tạo Huyện Văn Bàn'
-                    },
-                    { value: '10', title: 'Sở Giáo dục và Đào tạo Tỉnh Lào Cai' }
-                ], //chondonvi
+                selectedValueUnitEducation: [], //chondonvi
                 selectedValueGradeLevel: [], // choncaphoc
                 selectedValueSchool: [], //chontruonghoc
                 selectedValueSchoolYear: null //chonnamhoc
@@ -1019,20 +981,24 @@ export default {
             authToken: (state) => state.auth.token
         }),
 
+        dataChonDonVi_Store() {
+            return JSON.parse(localStorage.getItem('data_ChonDonVi'))
+        },
         activeThemeIcon() {
             return this.$store.state.activeThemeIcon
         }
     },
-
+    created() {
+        // Gán giá trị cho selectedValueUnitEducation ở đây
+        this.selectedValue.selectedValueUnitEducation =
+            this.dataChonDonVi_Store.map((item) => ({
+                title: item.tenDonVi,
+                value: item.maDonVi
+            }))
+    },
     mounted() {
         this.getDataChonDonVi()
         this.getDataChonTruonghoc()
-        // this.getDataThongKeTangGiam_TongSoTruongHoc()
-        // this.getDataThongKeTangGiam_TongSoGiaoVien()
-        // this.getDataThongKeTangGiam_TongSoHocSinh()
-        // this.getDataBieuDoChatLuongDaoTao_TruongHoc()
-
-        // this.getDataBieuDoDanhGiaHocSinh_HocLuc()
 
         // giá trị mặc định của chọn năm học
         const currentYear = new Date().getFullYear() - 2
